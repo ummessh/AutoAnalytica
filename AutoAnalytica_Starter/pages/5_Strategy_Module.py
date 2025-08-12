c7211b94221ecf1f9f4c4b8d60e39c668348da09
Umesh Chandra Mangesh Tiwari <132149893+ummessh@users.noreply.github.com>
2025-08-12 18:17:00 +0530

    Update 5_Strategy_Module.py

--- AutoAnalytica_Starter/pages/5_Strategy_Module.py
+++ AutoAnalytica_Starter/pages/5_Strategy_Module.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
-from prophet import Prophet
+from cmdstan_prophet import Prophet
 import plotly.express as px
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
